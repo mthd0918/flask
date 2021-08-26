e77d5aa71fab08fee87a396f524c3076b48dc972
hidechika <mthd0918@gmail.com>
2021-08-27 04:22:54 +0900

    flask section 7 & 8

---
+++ basic1.py
@@ -0,0 +1,15 @@
+# 標準入出力、コメント文、変数～基本講座１～
+
+# print 標準出力
+
+# input 標準入力
+name = input('あなたの名前は何ですか？')
+print(name)
+age = 18
+
+# format関数 変数の埋め込み
+print('私の名前は = {}, 年齢は{}' .format(name, age))
+
+a = b = 'hello'
+
+print(a, b)
