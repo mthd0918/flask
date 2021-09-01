c8700311af5046e5b830d50182f95407a90198a5
hidechika <mthd0918@gmail.com>
2021-09-01 20:47:22 +0900

    10-13

---
+++ basic/base5.py
@@ -0,0 +1,14 @@
+# 12. 数値（２進数、８進数、１６進数）～基本講座１～
+
+# 2, 8, 16進数
+age = 0b111  # 7
+print(age)
+age = 0o11  # 9
+print(age)
+age = 0x11  # 17
+print(age)
+
+# 2進数=>文字列
+print(bin(15))  # 0b1111
+print(oct(15))  # 0o17
+print(hex(15))  # 0xf
---
+++ basic/base7.py
@@ -0,0 +1,9 @@
+# 文字列型
+
+fruit = 'apple'
+print(fruit)
+
+print(type(fruit))
+
+print(fruit*10)
+print(fruit * 10)
